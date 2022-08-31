fbf7ca9141293a2dd259729d5cd504d875af30e6
neeryks <akashdeep.singh49@gmail.com>
2022-08-31 22:25:06 +0530

    API Build

--- fetch.py
+++ fetch.py
@@ -16,25 +16,31 @@ class fetcher(ShopifyUrls):
         sess = self.create_session()
         response = sess.get(self.url("get_orders"))
         data = response.json()
-        data = {"costumer_name":data["orders"][0]["shipping_address"]["first_name"]+data["orders"][0]["shipping_address"]["last_name"],
-        "city":data["orders"][0]["shipping_address"]["city"],
-        "state":data["orders"][0]["shipping_address"]["province"],
-        "country":data["orders"][0]["shipping_address"]["country"],
-        "address1":data["orders"][0]["shipping_address"]["address1"],
-        "address2":data["orders"][0]["shipping_address"]["address2"],
-        "pincode":data["orders"][0]["shipping_address"]["zip"],
-        "phone":data["orders"][0]["shipping_address"]["phone"],
-        "email":data["orders"][0]["contact_email"],
-        "order_id":data["orders"][0]["name"],
-        "order_id":data["orders"][0]["line_items"][0]["title"],
-        "design":data["orders"][0]["line_items"][0]['properties'][0]["value"],
-        "title":data["orders"][0]["line_items"][0]['properties'][1]["value"],
-        "datetime":data["orders"][0]["line_items"][0]['properties'][2]["value"],
-        "place":data["orders"][0]["line_items"][0]['properties'][3]["value"],
-        "coords":data["orders"][0]["line_items"][0]['properties'][4]["value"]
+        orders = []
+        for num_order in range(4):
+            print(num_order)
+            orders.append({"costumer_name":data["orders"][num_order]["shipping_address"]["first_name"]+data["orders"][num_order]["shipping_address"]["last_name"],
+            "city":data["orders"][num_order]["shipping_address"]["city"],
+            "state":data["orders"][num_order]["shipping_address"]["province"],
+            "country":data["orders"][num_order]["shipping_address"]["country"],
+            "address1":data["orders"][num_order]["shipping_address"]["address1"],
+            "address2":data["orders"][num_order]["shipping_address"]["address2"],
+            "pincode":data["orders"][num_order]["shipping_address"]["zip"],
+            "phone":data["orders"][num_order]["shipping_address"]["phone"],
+            "email":data["orders"][num_order]["contact_email"],
+            "order_id":data["orders"][num_order]["name"],
+            "order_name":data["orders"][num_order]["line_items"][0]["title"],
+            "design":data["orders"][num_order]["line_items"][0]['properties'][0]["value"],
+            "title":data["orders"][num_order]["line_items"][0]['properties'][1]["value"],
+            "datetime":data["orders"][num_order]["line_items"][0]['properties'][2]["value"],
+            "place":data["orders"][num_order]["line_items"][0]['properties'][3]["value"],
+            "coords":data["orders"][num_order]["line_items"][0]['properties'][4]["value"],
+            "amount":data["orders"][num_order]["line_items"][0]['properties'][4]["value"],
+            "variant":data["orders"][num_order]["shipping_address"]["variant_title"]
 
-        }
-        return data
+            })
+        
+        return orders
     
     def order_data(self):
         pass   
